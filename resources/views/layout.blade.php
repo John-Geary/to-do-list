a68bc06b667ec27e872ac2fdc32f766c885f9ec2
Ryan Mortier <ryan@mortier.ca>
2018-10-24 14:08:39 -0400

    Add scripts back in

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -121,7 +121,29 @@
 </div>
 
 <script src="{{ asset('/js/app.js') }}"></script>
-<script src="{{ asset('/js/jquery.js') }}"></script>
+<script>
+        $('#createTask').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget);
+            var group = button.data('group');
+            $(this).find('input#group').val(group);
+        });
+
+        $('#editGroup').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget);
+            var groupName = button.data('name');
+            var group = button.data('group');
+            $(this).find('.modal-body input').val(groupName);
+            $(this).find('.modal-content form').attr('action', '/groups/' + group);
+        });
+
+         $('#editTask').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget);
+            var taskName = button.data('name');
+            var task = button.data('group');
+            $(this).find('.modal-body input').val(taskName);
+            $(this).find('.modal-content form').attr('action', '/tasks/' + task);
+        });
+</script>
 
     </body>
 </html>
